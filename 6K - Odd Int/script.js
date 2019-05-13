47662b0971916daf6afe163c034f3a7f3e7fa615
Alejandro de Terán <alejandro.deteran@gmail.com>
2019-05-13 12:19:05 -0400

    Avance 'Odd Int'

--- 6K - Odd Int/script.js
+++ 6K - Odd Int/script.js
@@ -4,19 +4,26 @@ There will always be only one integer that appears an odd number of times.
 */
 
 function findOdd(A) {
-	let len = A.length;
-	let res = 0;
-	let temp = 0;
+	const len = A.length;	
+	const nums = {};
 	for(let i = 0; i < len; i++){
-				
+		nums[A[i]] = 0;
 	}
-	// console.log(res);
+	for(let i = 0; i < len; i++){
+		nums[A[i]]++;
+	}		
+	console.log(nums['1']);
+
+	Object.keys(nums).forEach((key,index) => {
+		console.log(key,nums[key])
+	})
 }
+
 
 let a = [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5];
 let b = [1,1,2,-2,5,2,4,4,-1,-2,5];
 let c = [20,1,1,2,2,3,3,5,5,4,20,4,5];
 
 findOdd(a);
-// findOdd(b);
-// findOdd(c);
+findOdd(b);
+findOdd(c);
